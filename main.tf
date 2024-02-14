cbe6b191c26c321e763c5d46d884d3cbdfa2ed29
shradhaacharya <58684826+shradhaacharya@users.noreply.github.com>
2024-02-14 18:15:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,10 +7,32 @@ resource "aws_vpc" "my_vpc" {
   tags       = var.tags
 }
 
-resource "aws_subnet" "my_subnets" {
-  count             = length(var.subnet_cidr_blocks)
+resource "aws_subnet" "public_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = var.subnet_cidr_blocks[count.index]
+  cidr_block        = var.public_subnet_cidr_block
   availability_zone = "${var.region}a"
-  tags              = merge(var.tags, { Name = "Subnet ${count.index + 1}" })
+  tags              = merge(var.tags, { Name = "Public Subnet" })
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = var.subnet_cidr_blocks[0] # Adjust as per your requirement
+  availability_zone = "${var.region}a"
+  tags              = merge(var.tags, { Name = "Private Subnet" })
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+
+  tags = merge(var.tags, { Name = "Public Route Table" })
+}
+
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
 }
