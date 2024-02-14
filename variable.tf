c0727c9649e906769c319a9a1fd73117d8438b30
shradhaacharya <58684826+shradhaacharya@users.noreply.github.com>
2024-02-14 18:02:34 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,23 @@
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidr_blocks" {
+  description = "CIDR blocks for the subnets"
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "region" {
+  description = "AWS region"
+  default     = "us-west-2"
+}
+
+variable "tags" {
+  description = "Tags for resources"
+  type        = map(string)
+  default     = {
+    Environment = "Production"
+    Application = "MyWebApp"
+  }
+}
