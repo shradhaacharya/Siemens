912ffc63a4056f7f78da144d8521508d52f37c67
shradhaacharya <58684826+shradhaacharya@users.noreply.github.com>
2024-02-14 18:31:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,3 +36,56 @@ resource "aws_route_table_association" "public_subnet_association" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_route_table.id
 }
+
+resource "aws_security_group" "lb_security_group" {
+  name        = "lb-security-group"
+  description = "Security group for load balancers"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "instance_security_group" {
+  name        = "instance-security-group"
+  description = "Security group for instances"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+   cidr_blocks = ["10.144.17.0/26"]  //Your-Management-IP-Range
+  }
+
+  ingress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["10.144.17.0/26"]  //Your-Management-IP-Rang
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
